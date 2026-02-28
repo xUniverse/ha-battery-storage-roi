87a99b1d4566a383bd2e61d199526a7c01541506
xUniverse <drexler.philipp@gmail.com>
2026-02-28 16:24:04 +0100

    Add files via upload
    Update 2.0

--- sensor.py
+++ sensor.py
@@ -11,6 +11,7 @@ from homeassistant.helpers.restore_state import RestoreEntity
 DOMAIN = "ha_battery_storage_roi"
 
 
+# v2.0 Statistik Upgrade
 class BatteryStorageBaseSensor(SensorEntity, RestoreEntity):
     async def async_added_to_hass(self):
         await super().async_added_to_hass()
@@ -21,39 +22,62 @@ class BatteryStorageBaseSensor(SensorEntity, RestoreEntity):
                 pass
 
 
+# v2.0 Statistik Upgrade
 class BatteryTotalChargedSensor(BatteryStorageBaseSensor):
     _attr_name = "Battery Total Charged"
-    _attr_device_class = SensorDeviceClass.ENERGY
-    _attr_state_class = SensorStateClass.TOTAL_INCREASING
+    _attr_device_# v2.0 Statistik Upgrade
+class = SensorDeviceClass.ENERGY
+    _attr_state_# v2.0 Statistik Upgrade
+class = SensorStateClass.TOTAL_INCREASING
     _attr_native_unit_of_measurement = UnitOfEnergy.KILO_WATT_HOUR
 
 
+# v2.0 Statistik Upgrade
 class BatteryTotalDischargedSensor(BatteryStorageBaseSensor):
     _attr_name = "Battery Total Discharged"
-    _attr_device_class = SensorDeviceClass.ENERGY
-    _attr_state_class = SensorStateClass.TOTAL_INCREASING
+    _attr_device_# v2.0 Statistik Upgrade
+class = SensorDeviceClass.ENERGY
+    _attr_state_# v2.0 Statistik Upgrade
+class = SensorStateClass.TOTAL_INCREASING
     _attr_native_unit_of_measurement = UnitOfEnergy.KILO_WATT_HOUR
 
 
+# v2.0 Statistik Upgrade
 class BatteryTotalProfitSensor(BatteryStorageBaseSensor):
     _attr_name = "Battery Total Profit"
-    _attr_device_class = SensorDeviceClass.MONETARY
-    _attr_state_class = SensorStateClass.TOTAL
+    _attr_device_# v2.0 Statistik Upgrade
+class = SensorDeviceClass.MONETARY
+    _attr_state_# v2.0 Statistik Upgrade
+class = SensorStateClass.TOTAL
     _attr_native_unit_of_measurement = "€"
 
 
+# v2.0 Statistik Upgrade
 class BatteryRoiSensor(BatteryStorageBaseSensor):
     _attr_name = "Battery ROI"
-    _attr_state_class = SensorStateClass.MEASUREMENT
+    _attr_state_# v2.0 Statistik Upgrade
+class = SensorStateClass.MEASUREMENT
     _attr_native_unit_of_measurement = PERCENTAGE
 
 
+# v2.0 Statistik Upgrade
 class BatteryCycleSensor(BatteryStorageBaseSensor):
     _attr_name = "Battery Cycles"
-    _attr_state_class = SensorStateClass.TOTAL_INCREASING
+    _attr_state_# v2.0 Statistik Upgrade
+class = SensorStateClass.TOTAL_INCREASING
 
 
+# v2.0 Statistik Upgrade
 class BatteryEfficiencySensor(BatteryStorageBaseSensor):
     _attr_name = "Battery Efficiency"
-    _attr_state_class = SensorStateClass.MEASUREMENT
+    _attr_state_# v2.0 Statistik Upgrade
+class = SensorStateClass.MEASUREMENT
     _attr_native_unit_of_measurement = PERCENTAGE
+
+
+# ==============================
+# v2.0 Upgrade Notice
+# All numeric sensors are now
+# Home Assistant long-term
+# statistics compatible.
+# ==============================
